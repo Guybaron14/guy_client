272ce2b7ce8739ae54d54e8c8b9811fbacb873c1
Guybaron14 <guybar135@gmail.com>
2022-03-27 00:36:15 +0300

    work from home

--- src/express/client/manager.ts
+++ src/express/client/manager.ts
@@ -3,19 +3,19 @@ import { ServerError } from '../error';
 import { IUser, INewUser } from './interface';
 
 const getUsers = async (query: Partial<IUser>): Promise<IUser[]> => {
-    return (await axios.get(`http://172.23.80.1:8000/api/users?firstName=${query.firstName}`)).data;
+    return (await axios.get(`http://172.29.64.1:8000/api/users?firstName=${query.firstName}`)).data;
 };
 
 const createUser = async (user: INewUser): Promise<IUser> => {
     try {
-        return (await axios.post('http://172.23.80.1:8000/api/users', user)).data;
+        return (await axios.post('http://172.29.64.1:8000/api/users', user)).data;
     } catch (err: any) {
         throw new ServerError(err.response.data.code, err.response.data.message);
     }
 };
 
 const deleteUser = async (id: string) => {
-    return (await axios.delete(`http://172.23.80.1:8000/api/users/${id}`)).data;
+    return (await axios.delete(`http://172.29.64.1:8000/api/users/${id}`)).data;
 };
 
 export { getUsers, createUser, deleteUser };
